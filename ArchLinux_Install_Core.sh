59be35ef945cc3c850d6513da4490d31fbfe262a
Keloud <toplaykisk@gmail.com>
2017-08-01 16:42:59 +0900

    URLの修正

--- ArchLinux_Install_Core.sh
+++ ArchLinux_Install_Core.sh
@@ -48,7 +48,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 ##Next Script
 echo echo -e "\033[0;33mNext Step\033[0;39m"
-wget -O https://github.com/keloud/MyLinuxScripts/blob/master/ArchLinux_Install/ArchLinux_Install_Sub.sh
+wget -O https://raw.githubusercontent.com/keloud/ArchLinux_InstallScript/master/ArchLinux_Install_Sub.sh
 chmod 7 Install_Sub.sh
 mv -f ./Install_Sub.sh /mnt/
 arch-chroot /mnt /bin/bash /Install_Sub.sh
